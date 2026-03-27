6b00a1745bb4170d388f096ef5048a1b75abed8c
Konrad Kleczkowski <konrad@kleczkow.ski>
2026-03-27 18:58:14 +0100

    Scaffold unified xq CLI crate
    Add crates/cli with the xq binary entry point and three placeholder
    subcommand modules (asm, dism, run).
    
    Implements QUI-313
    
    Signed-off-by: Konrad Kleczkowski <konrad@kleczkow.ski>

---
+++ crates/cli/src/asm.rs
@@ -0,0 +1,29 @@
+// Copyright (C) 2026 Postquant Labs Incorporated
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+//! `xq asm` subcommand -- assembles XQVM source into bytecode.
+
+use clap::Parser;
+
+/// Assemble an XQVM assembly source file into binary bytecode.
+#[derive(Debug, Parser)]
+pub(crate) struct Args {}
+
+/// Execute the `asm` subcommand.
+pub(crate) fn exec(_args: Args) -> miette::Result<()> {
+    todo!()
+}
